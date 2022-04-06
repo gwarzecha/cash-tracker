7a7edb6188683f3cd4a43932c7d15132f0e48dcc
gwarz <gmwarzecha@tutanota.com>
2022-04-05 21:10:59 -0700

    create a form on the login page with email and password

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,38 @@
-import styles from './Login.module.css';
+import { useState } from "react";
+
+// styles
+import styles from "./Login.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    // have to format login-form this way cuz of the hyphen
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 }
